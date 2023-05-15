6b94a3d20fd80d22b271395616dd6e93cfd1d018
Private Lap <abdel.gem@gmail.com>
2023-05-15 19:37:42 +0200

    Gerhald concept

--- Database.cpp
+++ Database.cpp
@@ -1,77 +1,20 @@
 #include "Database.hpp"
 #include <fmt/core.h>
 
-Database::Database(const std::string &command, const std::string& table) : connection(SqlCommands::startConnection.c_str())
+AbstractTable& Database::getMtvTable()
 {
-    tableName = table;
-    pqxx::work work(connection);
-    work.exec(command.c_str());
-    work.commit();
-}
-
-Database &Database::getInstanceMtv_1()
-{       
-   
-    std::string table = "tablex";
-    auto command = fmt::format(SqlCommands::createTable, table);
-    static Database instance_1{command, table};
-
+    static AbstractTable mtvTable;
     return instance_1;
 }
-Database &Database::getInstanceMtv_2()
-{   std::string table = "tabley";
-  
-     auto command = fmt::format(SqlCommands::createTable, table);
+AbstractTable &Database::getDocumentationTable()
+{
 
-    static Database instance_2{command, table};
+    static AbstractTable documentaionTable;
 
-    return instance_2;
+    return documentaionTable;
 }
 
-void Database::insertElement(int id, const std::string &value)
-{
-    auto command = fmt::format(SqlCommands::insertElement, tableName, id, value);
-    performExecuteCommand(command);
-}
 
-void Database::updateElement(int id, const std::string &value)
+Database::~Database()
 {
-    auto command = fmt::format(SqlCommands::updateElement, tableName, value, id);
-    performExecuteCommand(command);
-}
-
-std::string Database::getElement(int id)
-{
-    auto command = fmt::format(SqlCommands::findElement, tableName, id);
-    try
-    {
-        pqxx::work work(connection);
-        pqxx::result result = work.exec(command.c_str());
-
-        return result.at(0)["NAME"].c_str();
-    }
-    catch (const std::exception &error)
-    {
-        std::cerr << error.what() << std::endl;
-        return "NULL";
-    }
-}
-
-void Database::removeElement(int id)
-{
-    auto command = fmt::format(SqlCommands::removeElement, tableName, id);
-    performExecuteCommand(command);
-}
-
-void Database::deleteTable(const std::string &table)
-{
-    auto command = fmt::format(SqlCommands::deleteTable, table);
-    performExecuteCommand(command);
-}
-
-void Database::performExecuteCommand(const std::string &command)
-{
-    pqxx::work work(connection);
-    work.exec(command.c_str());
-    work.commit();
 }
--- Database.hpp
+++ Database.hpp
@@ -6,48 +6,26 @@
 #include <iostream>
 #include <pqxx/pqxx>
 #include "SqlCommands.hpp"
+#include "AbstractTable.hpp"
 
-/// @brief
+/**
+ *  @brief Diese Klasse ist ein Singleton und besitzt keine Kopier- oder Move-Konstruktoren.
+*  Um diese Klasse zu verwenden, muss die Methode "getInstance" aufgerufen werden.
+* Zum Beispiel: Database::getInstance().insertElement();
+*/
 class Database
 {
 
 public:
-    /// @brief
-    /// @return
-    static Database &getInstanceMtv_1();
-    static Database &getInstanceMtv_2();
-    /// @brief
-    /// @param id
-    /// @param value
-    void insertElement(int id, const std::string &value);
-    /// @brief
-    /// @param id
-    /// @param value
-    void updateElement(int id, const std::string &value);
-    /// @brief
-    /// @param id
-    /// @return
-    std::string getElement(int id);
-
-    /// @brief
-    /// @param id
-    void removeElement(int id);
-    /// @brief
-    /// @param taböe
-    void deleteTable(const std::string &table);
-
+    /* @brief
+    * Diese Methode konstruiert ein Objekt,
+    * das mit einer bestimmten Tabelle verknüpft ist und gibt dieses Objekt zurück.
+    * @return Ein Objekt, das mit der angegebenen Tabelle verknüpft ist.
+    */
+    static AbstractTable& getMtvTable();
+    static AbstractTable& getDocumentationTable();
+    virtual ~Database();
 private:
-    pqxx::connection connection;
-    /// @brief
-    /// @param
-    Database(const std::string &command, const std::string& table);
-    /// @brief
-    /// @param command
-    void performExecuteCommand(const std::string &command);
-
-
-    std::string tableName;
-
     // Delete copy and move operator also copy and move constructor to be able to implement singelton
     Database(Database &) = delete;
     Database(Database &&) = delete;
